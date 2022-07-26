784d84ec6cf4e4035ff3ea197a47f4bacdd271a5
jjbecker <loveforlife.jj@gmail.com>
2022-07-26 15:49:35 -0700

    Point to the latest integration result as the restart point

--- marbl_integrate.m
+++ marbl_integrate.m
@@ -62,10 +62,9 @@ marbl_file = 'Data/marbl_in'; % MARBL chemistry and other constants.
 
 %%%%%% INput restart file
 
-start_yr = 0;
-% inputRestartFile = 'Data/passive_restart_init.mat'; % from netCDF 5/25/22
-% start_yr = 113; inputRestartFile = 'Data_GP/restart_113.mat';
-start_yr = 4101; inputRestartFile = 'Data/InputFromAnn/restart4101.mat';
+% start_yr = 0; inputRestartFile = 'Data/passive_restart_init.mat'; % from netCDF 5/25/22
+start_yr = 70; inputRestartFile = 'Data_GP/restart_70.mat';
+% start_yr = 4101; inputRestartFile = 'Data/InputFromAnn/restart4101.mat';
 
 fprintf('%s.m: Reading OFFline input restart file with tracers and transports: %s\n', mfilename, inputRestartFile);
 load(inputRestartFile,'sim','MTM');
