c1d00ac5c643b7c7a3bef54bb7203c1ac57d780e
jjbecker <loveforlife.jj@gmail.com>
2022-09-07 09:17:49 -0700

    Many changes but moslty boild sown to removing broken subroutine "calc_f()" and replacing it with osmther tha works

--- calc_J_O2.m
+++ calc_J_O2.m
@@ -1,4 +1,5 @@
-function J = calc_J_O2(x0, sim, bgc, time_series, forcing, MTM)
+function J = calc_J_O2(sim, bgc, time_series, forcing, MTM)
+
 fprintf('%s.m: Start at %s\n', mfilename, datestr(datetime('now','TimeZone','local','Format','d-MMM-y HH:mm:ss Z')));
 %calc_J Summary of this function goes here
 %   Detailed explanation goes here
@@ -19,71 +20,75 @@ fprintf('%s.m: Start at %s\n', mfilename, datestr(datetime('now','TimeZone','loc
 % 3 index array:
 %
 %   first is location (aka water column),
-%   second is index of tendency
-%   third is index of tracer being changed
+%   second is index of water level
+%   third is index of water lever of same tracer tracer being changed
 %
 %   J(:,i,j) = d(MARBL(O2(i))) / d(tracer(O2(j)) @ all water col, lvl i,
 %   wrt O2 on lvl j
 
-%
-%   J(1,2,3) = d(MARBL(2)) / d(tracer(3) @ loc 1
-%
-%            = d(d(NO3)/dt)/ d(SiO3)     @ loc 1
+bgc_0 = bgc;
 
-fprintf('%s.m: Simplified Jacobian; takes ~600 seconds/tracer\n',mfilename);
+tName = tracer_names(0);    % no CISO tracers
+tStr = string(tName(sim.selection));
+fprintf('%s.m: Simplified Jacobian of "%s"; takes ~300 (s)\n',mfilename, tStr);
 
 tStart = tic;
 
 sz = size(bgc.tracer);
 J = zeros([sz(1) sz(2) sz(2)]);
 
+% calculate d(Tracer)/dt with tracer values x0
+% need f0; size(f0)= [7881,60,32]
+% f0 = calc_f(x0, sim, bgc, time_series, forcing, MTM,1);
+month = 1;
+
+[sim, bgc, time_series, n] = time_step_ann (sim, bgc, time_series, -1, forcing(month), MTM(month), month);
+
+f0 = bgc.tendency;
+f0 = squeeze(f0(:,:,sim.selection));   % size(f0) = [7881,60]
 
 % FIXME: always tricky to guess "delta"
-% FIXME: need to have correct uinit for h; what is that? median x0(iTr)??? or...
+% h = sqrt(eps) *x0 but need to convert x0 from FP to MARBL coordinate
+% size(h) =  [7881, 60, 1]
+% h = unpackMarbl(x0 *sqrt(eps), sim.domain.iwet_JJ,[sz(1),sz(2),1]);
 
-% h = x0 /1e3;
-% x0 = packMarbl(bgc.tracer,sim.domain.iwet_JJ);
-% x0 = x0(:,7);
-h = x0 *sqrt(eps);
-h = unpackMarbl(h, sim.domain.iwet_JJ,[7881,60,1]);
-% h = sqrt(eps) *ones(size(x0));
+h = squeeze(bgc.tracer(:,:,sim.selection));   % size(f0) = [7881,60]
+dh = eps + sqrt(eps) *h;
 
-% calculate d(Tracer)/dt with tracer values x0
-
-% tName = tracer_names(0);    % no CISO tracers
-f0 = calc_f(x0, sim, bgc, time_series, forcing, MTM,1);
 for lvl = 1:sz(2)       % loop over all lvl
 
-    dx = h*0;
-    dx(:,lvl) = h(:,lvl);
-    dx = dx(sim.domain.iwet_JJ);
+    %     dx = h*0;
+    %     dx(:,lvl) = h(:,lvl);
+    %     dx = dx(sim.domain.iwet_JJ);
+    %
+    %     x1 = x0 +dx;
+    %
+    %     % calculate d(Tracer)/dt with tracer values x1
+    %
+    %     f1 = calc_f(x1, sim, bgc, time_series, forcing, MTM,1);
 
-    x1 = x0 +dx;
+    bgc.tracer = bgc_0.tracer;
+    bgc.tracer (:, lvl, sim.selection) = bgc.tracer (:, lvl, sim.selection) +dh(:,lvl);
 
-    % calculate d(Tracer)/dt with tracer values x1
+    [sim, bgc, time_series, n] = time_step_ann (sim, bgc, time_series, -1, forcing(month), MTM(month), month);
 
-    f1 = calc_f(x1, sim, bgc, time_series, forcing, MTM,1);
-
+    f1 = bgc.tendency;
+    f1 = squeeze(f1(:,:,sim.selection));   % size(f1) = [7881,60]
     df = f1 -f0;
-    idx = 7;
+    df_dh = df ./ dh;  % size(df_dh) = [60 32] = d(tend(:,:))/dh(lvl,tr)
 
-    %     its too late to be cute, just use a loop...
-    J(:,:,lvl) = df(:,:,idx)./h(:,lvl);
-    %     for j=1:sz(2)
-    % %         J(:,j,lvl) = df(:,j) ./(dx+eps);
-    %         J(:,j,lvl) = df(:,j,idx)./h(:,j);
-    % %         if idx == 7
-    % %             fprintf('%s:m: norm of d(%s)\t/d(%s) = %.2g\n', mfilename, string(tName(j)), string(tName(idx)),norm(df(:,j)./dx));
-    % %         end
-    %     end
-    % https://duckduckgo.com/?q=181+This+sparse+indexing+expression+is+likely+to+be+slow.&t=osx&ia=web
+%     J(:,:,lvl) = df(:,:,sim.selection)./h(:,lvl);
+    J(:,:,lvl) = df_dh;
 end
 
 elapsedTime = toc(tStart);
-fprintf('%s.m: %1.3f (s) for partial of MARBL (O2 only) tendency WRT tracers (O2 only), on all levels of same column, and for all columns\n', mfilename, elapsedTime);
+fprintf('%s.m: %1.3f (s) for partial of MARBL tendency(%s) on all levels, w.r.t. (%s) of all levels of same column, for all columns\n', mfilename, elapsedTime, tStr, tStr);
 
 logJ=log10(abs(nonzeros(J(:))));
-figure(455); histogram(logJ); title("hist(log10(J))");
+figure(400+sim.selection); histogram(logJ); title(sprintf("hist( log10( abs( J( %s ))))",tStr), 'Interpreter', 'none');
+
+logJT=log10(sim.T *abs(nonzeros(J(:))));
+figure(500+sim.selection); histogram(logJT); title(sprintf("hist( log10( abs( sim.T *J( %s ))))",tStr), 'Interpreter', 'none');
 
 fprintf('End of %s.m: %s\n', mfilename, datestr(datetime('now','TimeZone','local','Format','d-MMM-y HH:mm:ss Z')));
 end
