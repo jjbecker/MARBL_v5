c45ff3136ef9a857e7bc2318d116e2a8914acad0
JJ Becker <loveforlife.jj@gmail.com>
2022-10-14 10:34:22 -0700

    Many fixes. Should be first operational single tracer solver with relax and foward

--- marbl_integrate.m
+++ marbl_integrate.m
@@ -12,68 +12,64 @@
 % "main" of cyclostationary transport version of MARBL,
 
 fprintf('%s.m: Start at %s\n', mfilename, datestr(datetime('now','TimeZone','local','Format','d-MMM-y HH:mm:ss Z')));
-
 % Need this to clear the "persistent" variables in "G()", "time_step()" and "calculate_forcing()"
 clear all;
-
 dbstop if error
 format short g
-
 addpath('MEX');
 addpath(genpath('utils'));
 addpath(genpath('plotting'));
 % dname = sprintf('%s/../',myDataDir());
 % addpath(dname);
-
 % % FIXME: diary behavior is such that if renamed it's still active diary!!
 diary off; diary off; diary on; diary off; diary on; diary on;
 % Clean up for threads, more complex and much slower than expected.
 % killAndCleanThreads();  % Leftover threads and dumps cause BIG trouble
 %%%%%%
-% Setup the big picture parts of a simulation and/or NK solution.
-timer_total = tic;
 
-% FIXME: Someday, when we know what inputs need to be, put all this a file
-time_step_hr = 3;
-phi_years    = 1;      % NK always using 1 year integration
+% Setup big picture parts of a simulation and/or NK solution.
 
-forwardIntegrationOnly = 0;
-recalculate_PQ_inv     = 1;
-num_r_iterations       = 3;
-num_forward_years      = 5;
-
-logTracers                = 1;
-captureAllSelectedTracers = 0;
-
-yearsBetweenRestartFiles  = 1;
-
-% FIXME: always need a selected tracer! For plot time series, or solve
+% always need a selected tracer! For plot time series, or solve
 % Most of these work very well in the single tracer solution...
 tName = tracer_names(0);    % no CISO tracers
-tracer_str = 'SiO3';
+tracer_str = 'DOCr';
 selection = [ find( strcmp(tName,tracer_str) ) ];
 % selection = [ ...
 %     find( strcmp(tName,'SiO3') ) ];     % #3
 %     find( strcmp(tName,'DOCr') ) ];     % #17
 
+forwardIntegrationOnly = 0; % 1 -> no NK just fwd integration
+recalculate_PQ_inv     = 0; % recalculate J, PQ,PQ_inv or load file
+% remember that "sol" is an x0 value. First relax iteration gives same x1 
+% as sol run. to be useful num_relax_iterations >= 2
+num_relax_iterations   = 1; % 0 means no relax steps, just use NK sol
+num_forward_years      = 2; % if fwd only, this is num fwd; 
+                            % else this is num fwd after relax , after sol
+logTracers                = 1;
+captureAllSelectedTracers = 0;
+yearsBetweenRestartFiles  = 1;
 
+% FIXME: Someday, when we know what inputs need to be, put all this a file
+time_step_hr = 3;
+phi_years    = 1;      % NK always using 1 year integration
 
 % DEBUG stuff
 logTracers = 1;
 time_step_hr = 12; % FAST debug
 
+timer_total = tic;
+
 %%%%%%
-
 marbl_file = 'Data/marbl_in'; % MARBL chemistry and other constants.
+%%%%%% 
 
-%%%%%% INput restart file
+% Input restart file
 
 % start_yr = 4101;inputRestartFileStem = 'Data/InputFromAnn/restart4101.mat';
 % start_yr =   0; inputRestartFileStem = 'Data/passive_restart_init.mat'; % from netCDF 5/25/22
 % start_yr = 260; inputRestartFile = 'Data_GP/restart_260_integrate_from_0.mat';
 % start_yr = 2525; inputRestartFileStem = 'restart_0_1_output/restart_260_NH4_x0_sol.mat';
-% start_yr = 3525; inputRestartFileStem = 'restart_0_1_output/restart_260_NH4_x0_sol.mat';
-start_yr = 3535; inputRestartFileStem = 'restart_0_1_output/restart_2535_Fe_x1.mat';
+start_yr = 3535; inputRestartFileStem = 'restart_0_1_output/restart_3536_Fe_x1.mat';
 
 inputRestartFile = strcat(myDataDir(), inputRestartFileStem);
 fprintf('%s.m: Reading OFFline input restart file with tracers and transports: %s\n', mfilename, inputRestartFile);
@@ -84,8 +80,7 @@ if ~isfile(inputRestartFile)
 end
 load(inputRestartFile,'sim','MTM');
 
-% We just over wrote sim struct, but now we can save the file stuff in it.
-% Need input restart name to make our output...
+% We just over wrote sim struct, so now we can save stuff in it again. sigh
 
 sim.forwardIntegrationOnly  = forwardIntegrationOnly ;
 sim.inputRestartFile        = inputRestartFile;
@@ -103,6 +98,8 @@ sim.checkNeg = 0;
 
 %%%%%% OUTput restart file
 
+% Need input restart name to make our output ???
+%
 % "restart file" -FROM- this OFFline sim for restart on Mac, not CESM.
 % Another hack is needed to move results from NK here back to CESM.
 
@@ -124,7 +121,8 @@ end
 
 sim.runInParallel = 1;
 if (sim.runInParallel)
-    sim.number_of_threads = 4; end
+    sim.number_of_threads = 12; % only 4 on laptop or 12 on GP supported
+end
 
 %%%%%%
 
@@ -214,13 +212,6 @@ c = reshape(c0, sz);
 x0 = c(:,sim.selection);    % initial condition for Nsoli()
 x0 = x0(:);                 % unitless
 
-
-selection = sim.selection;
-
-c0 = bgc2nsoli(sim, bgc.tracer);    % nsoli format; unitless; aka scaled FP
-sz = [ numel(sim.domain.iwet_JJ) , size(bgc.tracer,3) ];
-c = reshape(c0, sz);
-
 if( sim.forwardIntegrationOnly )
     fprintf('%s.m: forward integration ONLY\n',mfilename);
 else
@@ -232,98 +223,110 @@ else
         tStart = tic;
         %     keyboard
         load (strcat(myDataDir(),'sol/',strjoin(tName(sim.selection)),'_QJ'), 'PQ_inv')
-
+        
         elapsedTime = toc(tStart);
         fprintf('%s.m: %1.0f (s) to init sim and load PQinv \n',mfilename, toc(tStart));
-    end
+    end % calculate or load PQ_inv
+    
     fprintf('%s: Parameters nsoli()... \n', mfilename)
-
     % stop when norm is less than atol+rtol*norm of init_resid as seen by nsoli or brsola
     %
     % atol   = eps;           % sum of the squares in (1/s), IOW average error = 1/sec_y/379,913 = 8e-14 years
     % rtol   = 1e-7;          % stop when norm is less than atol+rtol*norm of init_resid as seen by nsoli
     %
-    % atol   = 1;
-    % rtol   = 1e-7;          % stop when norm is less than atol+rtol*norm of init_resid as seen by nsoli
-    %
     rtol   = 0;             % unfortunatley this is used with the initial drift, which is completely unknown
     atol   = norm(x0)*1e-6; % stop if norm(drift) < 1ppm of norm(x0)
-
+    atol   = norm(x0)*1e-2; % stop if norm(drift) < 1ppm of norm(x0)
+    
     tol    = [atol,rtol];   % [absolute error, relative tol]
     maxit  = 40;            % maximum number of nonlinear iterations (Newton steps) default = 40
     maxitl = 15;            % maximum number of Broyden iterations before restart, so maxdim-1 vectors are stored default = 40
-
+    
     % used only by nsoli()
     etamax = 0.9;           % maximum error tol for residual in inner iteration, default = 0.9
     lmeth  = 2;             % Nsoli() method 2 = GMRES(m), not used by brsola().
     restart_limit = 10;     % max number of restarts for GMRES if lmeth = 2, default = 20;
-
+    
     % first 2 of these parms are used by brsola, reat are specific to nsoli
     parms  = [maxit,maxitl,  etamax,lmeth,restart_limit];
-
+    
     [sol,it_hist,ierr,x_hist] = brsola(x0, @(x) calc_G(x,c0,sim,bgc,time_series,forcing,MTM,PQ_inv), tol, parms);
-
+    
+    % remember that "sol" is an x0 value...
+    
+    bgc_sol = bgc;
+    nsoli_sol = replaceSelectedTracers(sim, c0, sol, sim.selection);
+    bgc_sol.tracer = nsoli2bgc(sim, bgc_sol, nsoli_sol);
+    
     % sol_fname = strcat(string(tName(sim.selection)),'_sol');
     sol_fname = sprintf('%s_sol_ierr_%d', string(tName(sim.selection)), ierr)
     fprintf('%s.m: Saving sol, it_hist, ierr" in %s"...\n', mfilename,sol_fname);
     save(sol_fname, 'sol', 'it_hist','ierr','-v7.3','-nocompression');
-
+    
+    % Copy restart file phi() dropped and label it the solution file...
     myRestartFile = sprintf('%s/restart_%d_%s_x0.mat',             sim.outputRestartDir, round(sim.start_yr), strjoin(tName(sim.selection))      );
     mySolFile     = sprintf('%s/restart_%d_%s_x0_sol_ierr_%d.mat', sim.outputRestartDir, round(sim.start_yr), strjoin(tName(sim.selection)), ierr);
     fprintf('%s.m: Saving "%s" as the solution restart file "%s"...\n', mfilename,myRestartFile, mySolFile);
     copyfile( myRestartFile, mySolFile);
-
-    x = sol;
-    x_histx = x;
-    r_hist = zeros(size(x));
-    it_histx = zeros(num_r_iterations,1);
-    Npt = -1;
-
-
+   
+    
     fprintf('\n%s.m: Now we "relax" or forward integtration single variable solution a few iterations: %s\n',mfilename,datestr(datetime('now','TimeZone','local','Format','d-MMM-y HH:mm:ss Z')));
-    fprintf('%s.m: Starting Picard Integration for tracer selection = [%d]...\n',mfilename, sim.selection);
     fprintf('%s.m: %d time steps, each time step is %1.1f (h), simulating %1.1f years\n', ...
         mfilename, sim.T/sim.dt, sim.dt/sim.const.sec_h, tot_t /sim.const.sec_y)
-
-    for itc = 1:num_r_iterations
-        % FIXME: note "x" not "x0"
-
-        fprintf("\n%s.m: starting relaxation year #%d\n", mfilename, num_r_iterations)
-
-        [r,G, x1] = calc_G(x,c0,sim,bgc,time_series,forcing,MTM,PQ_inv);
-        % disp('FIXME: hacking r = -G')
-        % r = -G;
-        % fnrm = norm(r);
+    
+% remember that "sol" is an x0 value. First relax iteration gives same x1 
+% as sol run. to be useful num_relax_iterations >= 2
+    x = sol;
+%     x_histx = x;
+%     r_hist = zeros(size(x));
+%     it_histx = zeros(num_relax_iterations,1);
+    for itc = 1:num_relax_iterations
+        fprintf("\n%s.m: starting relaxation year #%d\n", mfilename, itc)
+        
+        % Note x = x1, not "x0" which is normal thing for sol iterations
+        [r,G, x1] = calc_G(x,c0,sim,bgc_sol,time_series,forcing,MTM,PQ_inv);
+       
+        % DEBUG
         fprintf('%s.m: itc %d norm(G) = %g\n',mfilename, itc, norm(G))
         fprintf('%s.m: itc %d norm(r) = %g\n',mfilename, itc, norm(r))
-
-        x_histx = [x_histx,x];
-        r_hist = [r_hist,r];
-        it_histx(itc,1) = norm(r);
-
+%         x_histx = [x_histx,x];
+%         r_hist  = [r_hist, r];       
+%         it_histx(itc,1) = norm(r);
+        
         x = x1;
-
-    end
-end
-
-
-% keyboard
-% sol = x;
-
-timer_loop = tic;
-years_gone_by = 0;
-for fwd_itc = 1:num_forward_years
-    fprintf("\n%s.m: starting forward integrate year #%d of %d\n", mfilename, fwd_itc, num_forward_years)
-    [sim, bgc, time_series, tracer_0] = phi(sim, bgc, time_series, forcing, MTM);
-
-    sim.start_yr = sim.start_yr+1;
-
-    years_gone_by = years_gone_by +1;
-    if mod(years_gone_by, sim.yearsBetweenRestartFiles) == 0    % This runs after last time step of every 10 y
-        % years gone by is actually the number of years-1 spent in phi.
-        [sim, bgc] = saveRestartFiles(sim, bgc, tracer_0, 0);
-    end
-end
+        
+    end % relax loop
+    
+    % If we relaxed sol, x is a relaxed x1.
+    %   if we did NOT rleax then x is sol = x0
+    bgc_relax = bgc;
+    nsoli_relax = replaceSelectedTracers(sim, c0, x, sim.selection);
+    bgc_relax.tracer = nsoli2bgc(sim, bgc_relax, nsoli_relax);
+    % keyboard
+    
+    
+    % Next! allow ALL tracers, not just selection, to "relax' to solution.
+    %    do pure forward integration for a while...
+    bgc_fwd = bgc_relax;
+    
+    timer_loop = tic;
+    years_gone_by = 0;
+    for fwd_itc = 1:num_forward_years
+        fprintf("\n%s.m: starting forward integrate year #%d of %d\n", mfilename, fwd_itc, num_forward_years)
+        [sim, bgc_fwd, time_series, tracer_0] = phi(sim, bgc_fwd, time_series, forcing, MTM);
+        
+        sim.start_yr = sim.start_yr+1;
+        
+        years_gone_by = years_gone_by +1;
+        if mod(years_gone_by, sim.yearsBetweenRestartFiles) == 0    % This runs after last time step of every 10 y
+            % years gone by is actually the number of years-1 spent in phi.
+            [sim, bgc_fwd] = saveRestartFiles(sim, bgc_fwd, tracer_0, 0);
+        end
+    end % fwd loop
+    
+    bgc = bgc_fwd; % this is in fact my final answer!
+    
+end % fwd only or solve, relax, fwd if statement
 
 disp([mfilename,' finished...'])
 elapsedTime_all_loc = toc(timer_loop);
