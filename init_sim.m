4e2cc9636763b33c0e4dd37ce64cf63cf7b8813e
jjbecker <loveforlife.jj@gmail.com>
2022-12-02 08:17:04 -0800

    Update init_sim.m
    Move tedious volume slice calculations and unit conversions to a new routine called calculate_depth_map_and_volumes() in utils . Als get rid of obsolete comments and commented code

--- init_sim.m
+++ init_sim.m
@@ -33,47 +33,8 @@ sim.T  = sim.num_time_steps * sim.dt;
 sim.tot_t = sim.dt*sim.num_time_steps;  % used only for debug output
 
 
-% Define gird dimensions in meters
-
-sim.domain.zt   = sim.grd.zt;
-sim.domain.zw   = sim.grd.zw+sim.grd.dzt; % in MARBL zw is bottom, not top...
-sim.domain.dzt  = sim.grd.dzt;
-sim.domain.dzw  = sim.grd.dzw;
-
-% ---> MARBL uses units of - CM - for depths, Matlab code uses meters!
-sim.domain.MARBL_depth_per_m = 100;     % 1 meter here is 100 cm MARBL uses
-
-% Create a struct that has meta data like size, names, and units.
-
-bgc_struct= init_bgc_struct(sim);
-
-
-[sim.domain.iwet_JJ, sim.domain.wet_loc, sim.domain.iwet_FP, sim.domain.num_wet_loc, ...
-    sim.domain.bottom_lvl, sim.domain.iColLvlFromFp] = createIwetEtc(sim.domain.M3d);
-
-% 2d matrix of "depth", e.g. 0:10900 meter <-- needed by MARBL
-% FIXME: is this actually used anywhere???
-% Only thing tricky is that Matlab array cant have index of 0...
-
-zw = cumsum(sim.grd.dzt')';     % bottom of grid, not depth at given local
-
-mapping = [1:length(zw); zw]';
-mapping = [0 0; mapping];           % add row for "dry" with indx == 0
-mapping(:, 1) = mapping(:, 1) +1;   % +1 for row with "dry" value
-LUT(mapping(:, 1)) = mapping(:, 2);
-sim.domain.bottom_depth = LUT(sim.domain.bottom_lvl+1);   % +1 for row with "dry" value
-
-sim.domain.dVt    = sim.grd.dV;
-sim.domain.dVt    = sim.domain.M3d .* sim.domain.dVt;
-sim.domain.V      =         sum(sim.domain.dVt, 'all') ;
-% with depth: layers get thicker, but area decreases. Volume per level
-% almost constant but sort of random
-sim.domain.V_lvl  = squeeze(sum(sim.domain.dVt, [1 2])); % vol at given iLvl
-sim.domain.V_lat  = squeeze(sum(sim.domain.dVt, [3 2])); % vol at given iLat
-sim.domain.V_lon  = squeeze(sum(sim.domain.dVt, [3 1])); % vol at given iLon
-
-sim.domain.dVt_FP = sim.domain.dVt(sim.domain.iwet_FP);
-
+% % % Define gird dimensions in meters
+[sim, bgc_struct] = calculate_depth_map_and_volumes(sim);
 %%%%%%%%%%%%%%
 
 % Initialize MARBL with "domain" aka grid dimensions.
@@ -98,21 +59,6 @@ else
     fprintf('\n\n%s.m: NOT running in parallel\n\n\n', mfilename);
 end
 
-% Need a local MARBL MEX running to get array sizes, even wthn looping over marbl_main.
-% HOWEVER, if running in parallel, local MARBL was killed last call here.
-% .. a real mess
-% persistent LOCAL_MARBL_IS_RUNNING
-% if isempty(LOCAL_MARBL_IS_RUNNING)
-%     LOCAL_MARBL_IS_RUNNING = 0;
-% end
-% if LOCAL_MARBL_IS_RUNNING == 1
-%     disp('local MEX -- IS -- already running, shut it down and then restart with init_marbl()')
-%     mex_marbl_driver('shutdown');
-%     disp('...local shutdown in persistent LOCAL_MARBL_IS_RUNNING worked.')
-%     LOCAL_MARBL_IS_RUNNING = 0;
-% end
-% disp('local MEX is -- NOT -- running, calling init_marbl()...')
-% LOCAL_MARBL_IS_RUNNING = 1;
 disp('Starting client MEX...')
 [sim, bgc_struct] = init_marbl(sim.marbl_file,sim, bgc_struct, forcing(1).surf_forcing);
 disp('...client MEX is running')
