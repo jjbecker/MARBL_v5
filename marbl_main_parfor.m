d4bf8d071b22245a91fef703cbdc42a6cc400b8c
jjbecker <loveforlife.jj@gmail.com>
2022-12-11 17:13:05 -0800

    starting to add a loop over single tracers

--- marbl_main_parfor.m
+++ marbl_main_parfor.m
@@ -39,47 +39,49 @@ sim.num_forward_years         = 0;      % if fwd only, num fwd, else this inum f
 
 % sim = setInputAndOutputFilePaths(sim, varargin)
 sim = setInputAndOutputFilePaths(sim,[]);
+sim.phi_years     = 1;      % NK always uses 1 year integration
+
 
 % FIXME: hack in some stuff for debug
 
-% sim.time_step_hr = 12;
+sim.time_step_hr = 12;
+% sim.recalculate_PQ_inv         = 0;
+% sim.debug_disable_phi          = 1;
+% sim.disable_ALL_Preconditioner = 1;
 
-sim.phi_years     = 1;      % NK always uses 1 year integration
 
 %%%%%%
+sim.grd         = load(sim.inputRestartFile,'sim').sim.grd;
+sim.domain      = load(sim.inputRestartFile,'sim').sim.domain;
+[sim, ~]        = calculate_depth_map_and_volumes(sim);
+
+sim  = setPeek(sim);
+
 if 0 && sim.debug_disable_phi
     fprintf('\n\n\t%s.m: ********* phi() is short circuited skip MTM read  *********\n\n',mfilename)
     MTM = 1;
 else
-    fprintf('%s.m: Loading the transports and tracers from restart file...\n', mfilename);
+    fprintf('%s.m: Loading transports and tracers from restart file...\n', mfilename);
     load(sim.inputRestartFile,'tracer','state','MTM');
     % MTM        = load(sim.inputRestartFile,'MTM').MTM;
     bgc.tracer = tracer;    clear tracer;
     bgc.state  = state;     clear state;
 end
+num_tr_selected = numel(sim.tracer_loop);
+sz_bgc          = [ numel(sim.domain.iwet_JJ) , size(bgc.tracer,3) ];
+
+%%%%% 
+% Need moles for calc_global_moles_and_means() for bgc2nsoli()
+% We need initial tracers for restart file[sim, 
 % FIXME: do we?
-% We need the initial tracers for the restart file[sim, 
-%%%%% Need moles for calc_global_moles_and_means() for bgc2nsoli()
-sim.grd     = load(sim.inputRestartFile,'sim').sim.grd;
-sim.domain  = load(sim.inputRestartFile,'sim').sim.domain;
-[sim, ~] = calculate_depth_map_and_volumes(sim);
-sim = setPeek(sim);
 sim = calc_global_moles_and_means(bgc, sim);
-
-c0 = bgc2nsoli(sim, bgc.tracer);    % nsoli format; unitless; aka scaled FP
-sz = [ numel(sim.domain.iwet_JJ) , size(bgc.tracer,3) ];
-% c  = reshape(c0, sz);
-% x0_bgc  = replaceSelectedTracers(sim, c0, x0_sol, sim.selection);
-
-%%%%%%% These are the output result from the "parfor slice variable", sized to accept all possible tracers
-num_tr = numel(sim.tracer_loop);
-singleColTracerError    = zeros([1, size(bgc.tracer,3)]);
-singleColTracerSolution = zeros([size(sim.domain.iwet_FP,1), size(bgc.tracer,3)]);
+c0  = bgc2nsoli(sim, bgc.tracer);    % nsoli format; unitless; aka scaled FP
+c   = reshape(c0, sz_bgc);
 
 %%%%%%
 delete(gcp('nocreate'))
 numCores = max(round(feature('numcores')/2), 6) % Never more than 6
-numCores = min(num_tr, numCores)                % Never more than numTracer
+numCores = min(num_tr_selected, numCores)                % Never more than numTracer
 if isunix && ismac
     numCores = min(2, numCores)     % hack for limited RAM on laptop
 end
@@ -90,38 +92,92 @@ end
 ticBytes(gcp);
 
 
-sim.recalculate_PQ_inv         = 0;
-sim.debug_disable_phi          = 1;
-sim.disable_ALL_Preconditioner = 1;
 sim
-% keyboard
+keyboard
 myFilename = 'marbl_main_parfor';     % mfilename does NOT work in a parfor
-parfor (par_idx = 1:num_tr,numCores)              % PARENTHESIS are CRUCIAL
-% for par_idx = 1:num_tr
+%%%%%%% 
+% These are output result from "parfor slice variable", sized to accept all
+% possible tracers.
 
-    tmp_sim = sim;      % 'tmp_sim'  may be extraneous; parfor crazy!
+singleColTracerError = zeros([1, size(bgc.tracer,3)]);
+% singleColTracerSolution = 0 *c;
+% FIXME: these temps DO work in loop below but end up inscrambled order...
+tmp_xsol = zeros([size(sim.domain.iwet_FP,1), size(bgc.tracer,3)]);
+tmp_ierr = zeros([1, size(bgc.tracer,3)]);
 
-    tracer_cell = tmp_sim.tracer_loop     (par_idx);
-    tracer_col  = tmp_sim.tracer_loop_idx (par_idx);
-    fprintf('%s.m: Starting tracer #%d (%s)\n', myFilename, tracer_col, string(tracer_cell))
+%%%
 
-%     [tmp_sim, tmp_bgc, tmp_ierr, tmp_x] = parfor_inner(tmp_sim, MTM, tracer_cell );
-        [~, ~, ierr, x] = parfor_inner(tmp_sim, MTM, tracer_cell );
-%         singleColTracerError   (:, tracer_col) = ierr;
-%         singleColTracerSolution(:, tracer_col) = x;
+parforIdxRange = 1:num_tr_selected;
+
+% par_idx is simply "order of execution" -NOT- tracer number
+% so we nee3d tracerRange which is tracer number.
+% 
+% have to be SOOOOOO careful to avoid brodcasting huge struct. Using
+% sim.tracer_loop will be a wasted copy of a huge sim struct.
+tracerRange = sim.tracer_loop_idx;
+tracer_cell = sim.tracer_loop;
+
+% for par_idx = 1:3  % just for DEBUG
+parfor (par_idx = parforIdxRange,numCores)  % PARENTHESIS are CRUCIAL
+
+    % Also crucial: par_idx order randomly selected from range!
+
+    % DEBUG have to be SOOOOOO careful to avoid brodcasting huge struct
+    % tmp_sim = sim; %% NOT an issue since we have send whold sim anyway? 
+    fprintf('%s.m: Starting tracer parfor idx %d which is for tracer %s (#%d)\n', myFilename, ...
+        par_idx, string(tracer_cell(par_idx)), tracerRange (par_idx))
+
+    % Works, but lots of code to catch two array, but easy to read...
+    
+    [~, ~, ierr, xsol] = parfor_inner(sim, MTM, string(tracer_cell(par_idx)));
+    
+    tmp_ierr (:, par_idx) = ierr;
+    tmp_xsol (:, par_idx) = xsol;
+
+    % Works, but hard to read...
+    %
+    % [~, ~, tmp_ierr(:,par_idx), tmp_xsol(:,par_idx)] = ...
+    %     parfor_inner(sim, MTM, string(tracer_cell(par_idx)));
+    % 
+    %
+    % FIXME: does NOT work, something about slicing. Most likely can only 
+    % use parfor index, not one computed from it
+    %
+    % tracer_num  = tracerRange (par_idx);
+    % singleColTracerError   (:, tracer_num) = ierr;
+    % singleColTracerSolution(:, tracer_num) = x;
+    
 end % of loop over tracers
+
+fprintf('...end of loop over tracers : '); toc(timer_total)
+fprintf('Shutting down parpool...\n')
 
 tocBytes(gcp)
 delete(gcp('nocreate'))
 
-fprintf('...end of loop over tracers : '); toc(timer_total)
-fprintf('Shutting down the parpool...\n')
+%%%
 
-% bgc.tracer = nsoli2bgc(sim, bgc, x0_bgc);   % marbl format x0
+% Now unscramble results captured in random order by parfor loop. No need
+% for a for loop, use array index on slices.
 
+tracerRange = sim.tracer_loop_idx (parforIdxRange);
+
+singleColTracerError   (:, tracerRange) = tmp_ierr (:, parforIdxRange);
+% singleColTracerSolution(:, tracerRange) = tmp_xsol (:, parforIdxRange);
+c(:, tracerRange)                       = tmp_xsol (:, parforIdxRange); 
+
+% Just replaced initial values in restart with single column solution. Now
+% just need to create a restart file with this result.
+
+keyboard
+[~,oldName,~] = fileparts(sim.inputRestartFile)
+newRestartFileName = sprintf('%s/%s_single_col_%s.mat', sim.outputRestartDir, oldName, strjoin(tracer_cell,'_'))
+[sim, bgc] = saveRestartFiles(sim, bgc, c, newRestartFileName);
+
+%%%%% whew. 
 
 % FIXME: need to save workspace?!
-logDir = strcat(sim.outputRestartDir,'/Logs/');
+    logDir = strcat(sim.outputRestartDir,'/Logs/');
 if ~exist(logDir, 'dir')
     mkdir(logDir)
 end
