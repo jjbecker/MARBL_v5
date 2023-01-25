34024266963559d7c15dd9eca8eff4e27c722640
jjbecker <loveforlife.jj@gmail.com>
2023-01-24 17:05:00 -0800

    fix typos

--- plotting/plot_log.m
+++ plotting/plot_log.m
@@ -22,7 +22,7 @@ for i=1:size(y_array,2)
         hold on
         plot([t_or_z(1) t_or_z(end)],[y_array(1,i) y_array(end,i)])
         if floor(t_or_z(end)/365) >0
-            xline([1:floor(t_or_z(end)/365)]*365)% fig = 6;
+            xline([1:floor(t_or_z(end)/365)]*365);
         end
         hold off
     end
